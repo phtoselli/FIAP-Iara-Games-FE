7af252991fc4664e711c9317be2912ac68c9447d
phtoselli <phtoselli@gmail.com>
2024-11-10 23:14:48 -0300

    feat: create gender

--- src/modules/Home.jsx
+++ src/modules/Home.jsx
@@ -16,6 +16,13 @@ import a1 from '../assets/a1.svg';
 import a2 from '../assets/a2.svg';
 import a3 from '../assets/a3.svg';
 
+import g1 from '../assets/g1.png';
+import g2 from '../assets/g2.png';
+import g3 from '../assets/g3.png';
+import g4 from '../assets/g4.png';
+import g5 from '../assets/g5.png';
+import g6 from '../assets/g6.png';
+
 export default function Home() {
   // const navigate = useNavigate();
 
@@ -86,11 +93,50 @@ export default function Home() {
 
       {/* --------------- GENDER --------------- */}
       <Flex vertical="vertical" align="center" justify="center" style={{width: '100%'}}>
-        <Flex vertical="vertical" align="center" justify="center" gap={32} style={{width: '800px', maxWidth: '800px', padding: '80px 0px'}}>
+        <Flex vertical="vertical" align="center" justify="center" style={{width: '800px', maxWidth: '800px', padding: '80px 0px'}}>
           <Flex justify="space-between" style={{width: '100%'}}>
-              <Title level={2}>Gêneros</Title>
-              <Link to="/genders">Ver todos</Link>
-            </Flex>
+            <Title level={2}>Gêneros</Title>
+            <Link to="/genders">Ver todos</Link>
+          </Flex>
+
+          <Row style={{width: '100%'}}>
+            <Col span={4} style={{padding: '8px', height: '120px'}}>
+              <Flex align="center" justify="center" style={{borderRadius: '4px', backgroundColor: '#444', width: '100%', height: '100%', backgroundImage: `url(${g1})`,  backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center'}}>
+                <Paragraph style={{color: '#ffffff'}}>Estratégia</Paragraph>
+              </Flex>
+            </Col>
+            <Col span={4} style={{padding: '8px', height: '120px'}}>
+              <Flex align="center" justify="center" style={{borderRadius: '4px', backgroundColor: '#444', width: '100%', height: '100%', backgroundImage: `url(${g2})`,  backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center'}}>
+                <Paragraph style={{color: '#ffffff'}}>FPS</Paragraph>
+              </Flex>
+            </Col>
+            <Col span={4} style={{padding: '8px', height: '120px'}}>
+              <Flex align="center" justify="center" style={{borderRadius: '4px', backgroundColor: '#444', width: '100%', height: '100%', backgroundImage: `url(${g3})`,  backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center'}}>
+                <Paragraph style={{color: '#ffffff'}}>RPG</Paragraph>
+              </Flex>
+            </Col>
+            <Col span={4} style={{padding: '8px', height: '120px'}}>
+              <Flex align="center" justify="center" style={{borderRadius: '4px', backgroundColor: '#444', width: '100%', height: '100%', backgroundImage: `url(${g4})`,  backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center'}}>
+                <Paragraph style={{color: '#ffffff'}}>Puzzle</Paragraph>
+              </Flex>
+            </Col>
+            <Col span={4} style={{padding: '8px', height: '120px'}}>
+              <Flex align="center" justify="center" style={{borderRadius: '4px', backgroundColor: '#444', width: '100%', height: '100%', backgroundImage: `url(${g5})`,  backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center'}}>
+                <Paragraph style={{color: '#ffffff'}}>Role Playing</Paragraph>
+              </Flex>
+            </Col>
+            <Col span={4} style={{padding: '8px', height: '120px'}}>
+              <Flex align="center" justify="center" style={{borderRadius: '4px', backgroundColor: '#444', width: '100%', height: '100%', backgroundImage: `url(${g6})`,  backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center'}}>
+                <Paragraph style={{color: '#ffffff'}}>Esportes</Paragraph>
+              </Flex>
+            </Col>
+          </Row>
         </Flex>
       </Flex>
 
@@ -100,7 +146,7 @@ export default function Home() {
         <Flex vertical="vertical" align="center" justify="center" gap={32} style={{width: '800px', maxWidth: '800px', padding: '80px 0px'}}>
         <Flex justify="space-between" style={{width: '100%'}}>
             <Title level={2}>Lançamentos</Title>
-            <Link to="/genders">Ver todos</Link>
+            <Link to="/launch">Ver todos</Link>
           </Flex>
         </Flex>
       </Flex>
@@ -111,7 +157,7 @@ export default function Home() {
         <Flex vertical="vertical" align="center" justify="center" gap={32} style={{width: '800px', maxWidth: '800px', padding: '80px 0px'}}>
         <Flex justify="space-between" style={{width: '100%'}}>
             <Title level={2}>Jogos Populares</Title>
-            <Link to="/genders">Ver todos</Link>
+            <Link to="/popular">Ver todos</Link>
           </Flex>
         </Flex>
       </Flex>
